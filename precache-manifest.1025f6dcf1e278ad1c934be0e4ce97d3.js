010de5d5e680e7d34b25d171ddb10a52521eaced
Kropi-H <kropacek.h@gmail.com>
2020-08-15 19:40:52 +0200

    Updates

---
+++ precache-manifest.1025f6dcf1e278ad1c934be0e4ce97d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a1b433a45f6dba657573b28ae95441",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "ec110ee6cb3fa9b83201",
+    "url": "/facerecognitionbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "93b1c938ed235cb5c22a",
+    "url": "/facerecognitionbrain/static/css/main.7309531d.chunk.css"
+  },
+  {
+    "revision": "ec110ee6cb3fa9b83201",
+    "url": "/facerecognitionbrain/static/js/2.1352e33a.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognitionbrain/static/js/2.1352e33a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b1c938ed235cb5c22a",
+    "url": "/facerecognitionbrain/static/js/main.cbc18ae7.chunk.js"
+  },
+  {
+    "revision": "a0d072c450e7e6f545a0",
+    "url": "/facerecognitionbrain/static/js/runtime-main.3b236050.js"
+  }
+]);
